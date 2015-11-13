90bb6336132ccd13078e16b82fe0cc82af4ec122
Manuel João <mjsrs@sapo.pt>
2015-11-13 20:57:30 +0000

    added arrow up/down to get old commands; focus input box after entering a command

--- main.js
+++ main.js
@@ -3,14 +3,16 @@
 /*eslint no-console: 0*/
 
 var timer;
+var last_command = {index:-1, cmds:[]};
 var allowed_commands = ['help', 'getinfo', 'masternode count'];
 var presence = new Firebase("https://masternode.firebaseio.com/presence/");
 var user = presence.push({ timestamp:  Firebase.ServerValue.TIMESTAMP, request: '', response: ''});
 var commands = new Firebase("https://masternode.firebaseio.com/presence/" + user.key() + '/');
 var connection = new Firebase("https://masternode.firebaseio.com/.info/connected");
 
-$('#cmd_box').keypress(function(event) {
-    if (event.which === 13) {
+$('#cmd_box').keydown(function(event) {
+    switch(event.which){
+    case 13:
         event.preventDefault();
         console.log('enter key pressed');
         console.log(event.currentTarget.value);
@@ -21,11 +23,29 @@ $('#cmd_box').keypress(function(event) {
         event.currentTarget.disabled = true;
         //update record
         user.update({ timestamp:  Firebase.ServerValue.TIMESTAMP, request: cmd, response: ''});
+        //add command to last commands buffer
+        last_command.cmds.unshift(cmd);
         //start server timeout
         timer = setTimeout(function(){
             $('p[id=response]').html('Error - server response timeout');
             $('#cmd_box').removeAttr('disabled');
+            $('#cmd_box').focus();
         }, 3000);
+        break;
+    //arrow up -> navigate recent commands
+    case 38:
+        if (last_command.index + 1 <= last_command.cmds.length - 1) {
+            last_command.index += 1;
+            event.currentTarget.value = last_command.cmds[last_command.index];
+        }
+        break;
+    //arrow down -> navigate recent commands
+    case 40:
+        if (last_command.index - 1 >= 0) {
+            last_command.index -= 1;
+            event.currentTarget.value = last_command.cmds[last_command.index];
+        }
+        break;
     }
 });
 
@@ -43,6 +63,7 @@ user.on('child_changed', function(snap) {
         console.log(snap.key() + ':' +snap.val());
         $('p[id=response]').html(snap.val());
         $('#cmd_box').removeAttr('disabled');
+        $('#cmd_box').focus();
     }
 });
 
