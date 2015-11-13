57ff8c4324b8ac9440222b661d72856db772212e
Manuel João <mjsrs@sapo.pt>
2015-11-13 09:22:07 +0000

    initial commit

---
+++ cmds.js
@@ -0,0 +1,16 @@
+'use strict';
+/* globals exports*/
+
+exports.public = [
+    'help',
+    'masternode count',
+    'getinfo',
+    'masternodelist',
+    'mnbudget show',
+    'mnbudget projection',
+    'mnbudget check',
+    'mnbudget nextblock',
+    'mnfinalbudget show',
+    'spork show',
+    'spork active'
+];
---
+++ masternode.js
@@ -0,0 +1,39 @@
+'use strict';
+/* globals require*/
+/*eslint no-console: 0*/
+var Firebase = require('firebase');          //https://www.npmjs.com/package/firebase
+var exec = require('child_process').exec;
+var cmds = require('./cmds');
+
+var url = 'https://masternode.firebaseio.com/presence/';
+var masternode = new Firebase(url);
+
+masternode.on('child_changed', function(child) {
+    if (child.val().request === '') {return;}
+
+    console.log(Date() + 'cmd received: ' + child.val().request);
+
+    //is cmd in public commands?
+    var index = cmds.public.indexOf(child.val().request);
+    if (index >= 0) {
+        var cmd = 'dash-cli -datadir=/opt/data -conf=/opt/data/dash.conf ' + cmds.public[index];
+        //execute requested command
+        exec(cmd, 
+            function(error, stdout, stderr) {
+                if (error !== null) {
+                    console.log(error);
+                    //return command error
+                    child.ref().update({response: stderr});
+                    return;
+                }else{
+                    //return command output
+                    child.ref().update({response: stdout});
+                }
+            }
+        );
+    }else{
+        //return command not available
+        child.ref().update({response: 'Command not available.'});
+        console.log('Command not available.');
+    }
+});
